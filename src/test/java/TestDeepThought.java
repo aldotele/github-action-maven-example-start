470a8e9740b79f5620d151d8fa147595fdf479ae
aldotele <aldo.telese@hotmail.it>
2022-10-05 22:38:59 +0200

    fixes test

--- src/test/java/TestDeepThought.java
+++ src/test/java/TestDeepThought.java
@@ -11,7 +11,6 @@ public class TestDeepThought {
     @Test
     @DisplayName("Test the answer to everything")
     void testAnswerToAll(){
-        assertEquals(1, 10);
         DeepThought superComputer = new DeepThought();
         assertEquals( 42 , superComputer.answer_to_the_ultimate_question_of_life_the_universe_and_everything());
     }
